4c86390d65aed5f59c33ed4613550ff22406eaee
Will <wwatkins102@gmail.com>
2016-08-04 14:32:41 -0400

    Added Auto_LTSpice_Now source code

---
+++ final/src/Auto_LTS_2.h
@@ -0,0 +1,526 @@
+#ifndef AUTO_LTS_2_H
+#define AUTO_LTS_2_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+#include<math.h>
+#include<ctype.h>
+#include<sys/stat.h>
+
+/*
+ * This struct stores the information
+ * characterizing the paramters that
+ * will be incremented. ext_param_name
+ * & ext_param_str are pointers to arrays
+ * of char* strings. ext_param_step_array
+ * is an array of arrays, so that each array
+ * characterizes the lowest value, the highest
+ * value and the step size for that nth parameter
+ * i.e. ext_param_step_array[parameter][low=0|high=1|step=2]
+ * ext_param_data_array is an array made up of each
+ * value that each paramter can take. This array's
+ * values are created with the generate_data_array
+ * function. ext_param_permutations gives the combinations
+ * of each data array, so that iterating through ext_param_permutations
+ * produces the unique paramter values for that simulation.
+ * ext_param_index is an array of the char indices that each
+ * parameter is defined on the .param line for so that later 
+ * each new paramter value obtained from ext_param_permutations
+ * can be subbed into the correct place on the .param spice directive.
+ */
+ 
+enum {
+	MAX_CHAR = 255
+};
+
+char MAX_CURRENT[] = "-5m";
+char MIN_CURRENT[] = "0";
+char IN_PULSE[] = "InPulse";
+char IN_REF_PULSE[] = "InRefPulse";
+char RISE[] = "100p";
+char FALL[] = "100p";
+char ON[] = "1.3n";
+
+struct netlist
+{
+	char **param_name;
+	char **param_strvalue;
+	char **vin_str;
+	char **save_str;
+	
+	float *param_fvalue;
+	
+	char tran_str[MAX_CHAR];
+	char ac_str[MAX_CHAR];
+	
+	long tran_pos;
+	long ac_pos;
+	
+	char in_label[MAX_CHAR];
+	char in_ref_label[MAX_CHAR];
+	char v_rise[MAX_CHAR];
+	char v_fall[MAX_CHAR];
+	char v_on[MAX_CHAR];
+	char ac_amplitude[MAX_CHAR];
+	float delta_delay;
+	char **delay;
+	
+	int is_tran;
+
+	int save_size;
+	int num_save;
+	
+	int num_currents;
+	
+	int num_param;
+	int param_size;
+}; 
+
+/* Function prototypes */
+int initialize_netlist(struct netlist *net, int argc, char **argv);
+int realloc_netlist(struct netlist *net);
+int free_netlist(struct netlist *net);
+float str_to_expo(char *str);
+int expo_to_str(char *str, float expo);
+int prepare_netlist(struct netlist *net, FILE *tl_netlist);
+
+/*
+ * Dynamically allocates memory for the struct
+ * as it is unknown at compile-time how much
+ * memory to allocate for arrays storing information
+ * on the stepped parameters
+ */
+int initialize_netlist(struct netlist *net, int argc, char **argv)
+{
+	printf("Allocating netlist memory...\n");
+	
+	net->param_size = 3;
+	net->num_param = 0;
+
+	net->param_name = malloc(net->param_size*sizeof(char*));
+	net->param_strvalue = malloc(net->param_size*sizeof(char*));
+	net->param_fvalue = malloc(net->param_size*sizeof(float));
+	for(int n=0; n < net->param_size; n++) {
+		net->param_name[n] = malloc(MAX_CHAR*sizeof(char));
+		net->param_strvalue[n] = malloc(MAX_CHAR*sizeof(char));
+	}	
+	
+	printf("Determining the number of pulses...\n");
+	int curr_index = 0;
+	int max_size = 5;
+	char *p_tok;
+	p_tok = strtok(argv[9], " ");
+	net->vin_str = malloc(max_size*sizeof(char*));
+	while(p_tok != NULL) {
+		if(curr_index >= max_size) {
+			max_size += 3;
+			net->vin_str = realloc(net->vin_str, max_size*sizeof(char*));
+		}
+		net->vin_str[curr_index] = malloc(MAX_CHAR*sizeof(char));
+		strcpy(net->vin_str[curr_index], p_tok);
+		curr_index++;
+		p_tok = strtok(NULL, " ");
+	}
+	net->num_currents = curr_index;
+	for(int n=curr_index; n < max_size; n++) 
+		free(net->vin_str[n]);
+	net->vin_str = realloc(net->vin_str, net->num_currents*sizeof(char*));
+	net->delta_delay = 20e-9;
+	net->delay = malloc(net->num_currents*sizeof(char*));
+	printf("Determining the delay between pulses...\n");
+	float delay_tmp = 0;
+	for(int n=0; n < net->num_currents; n++) {
+		net->delay[n] = malloc(MAX_CHAR*sizeof(char));
+		expo_to_str(net->delay[n], delay_tmp);
+		printf("Delay[%d] = %s\n", n, net->delay[n]);
+		delay_tmp += net->delta_delay;
+	}
+
+	strcpy(net->in_label, IN_PULSE);
+	strcpy(net->in_ref_label, IN_REF_PULSE);
+	strcpy(net->v_rise, RISE);
+	strcpy(net->v_fall, FALL);
+	strcpy(net->v_on, ON);
+	strcpy(net->ac_amplitude, "1");
+	
+	net->is_tran = 0;
+	if((strcmp(argv[2], "tran")==0) || (strcmp(argv[2], "TRAN")==0))
+		net->is_tran = 1;
+
+	net->save_size = 8;
+	net->num_save = 0;
+	net->save_str = malloc(net->save_size*sizeof(char*));
+	for(int n=0; n < net->save_size; n++)
+		net->save_str[n] = malloc(MAX_CHAR*sizeof(char));
+	
+	return 0;
+}
+
+/*
+ * Reallocates the struct's memory blocks
+ * when the number of parameters to be stepped
+ * is updated. 
+ */
+int realloc_netlist(struct netlist *net)
+{
+	printf("Reallocating netlist param string memory...\n");	
+	net->param_size += 3;
+	
+	net->param_name = realloc(net->param_name, net->param_size*sizeof(char*));
+	net->param_strvalue = realloc(net->param_strvalue, net->param_size*sizeof(char*));
+	net->param_fvalue = realloc(net->param_fvalue, net->param_size*sizeof(float));
+	for(int n=net->num_param; n < net->param_size; n++) {
+		net->param_name[n] = malloc(MAX_CHAR*sizeof(char));
+		net->param_strvalue[n] = malloc(MAX_CHAR*sizeof(char));
+	}
+
+	return 0;
+}
+
+int realloc_save(struct netlist *net)
+{
+	printf("Reallocating netlist save string memory...\n");
+	net->save_size += 3;
+	
+	net->save_str = realloc(net->save_str, net->save_size*sizeof(char*));
+	for(int n=net->num_save; n < net->save_size; n++) 
+		net->save_str[n] = malloc(MAX_CHAR*sizeof(char));
+	
+	return 0;
+}
+
+/*
+ * Frees the struct's memory blocks previously 
+ * dynamically allocated by initialize_params
+ * & realloc_params. Should be used if the struct
+ * will not be used any more to prevent memory
+ * leakage.
+ */
+int free_netlist(struct netlist *net)
+{
+	for(int n=0; n < net->num_param; n++) {
+		free(net->param_name[n]);
+		free(net->param_strvalue[n]);
+	}
+	free(net->param_name);
+	free(net->param_strvalue);
+	free(net->param_fvalue);
+
+	return 0;
+}
+
+float str_to_expo(char *str)
+{
+	char *pch;
+	float expo = strtof(str, &pch);
+	switch(*pch) {
+		case 'Y': expo *= 1E24; break;
+		case 'Z': expo *= 1E21; break;
+		case 'E': expo *= 1E18; break;
+		case 'P': expo *= 1E15; break;
+		case 'T': expo *= 1E12; break;
+		case 'G': expo *= 1E9; break;
+		case 'M': expo *= 1E6; break;
+		case 'k': expo *= 1E3; break;
+		case 'd': expo *= 1E-1; break;
+		case 'c': expo *= 1E-2; break;
+		case 'm': expo *= 1E-3; break;
+		case 'u': expo *= 1E-6; break;
+		case 'n': expo *= 1E-9; break;
+		case 'p': expo *= 1E-12; break;
+		case 'f': expo *= 1E-15; break;
+		case 'a': expo *= 1E-18; break;
+		case 'z': expo *= 1E-21; break;
+		case 'y': expo *= 1E-24; break;
+	}
+	return expo;
+}
+
+/*
+ * Converts a float in scientific notation
+ * i.e. exponent notation into a string
+ * appended with: m(illi), k(ilo), etc.
+ * One exception is for mega, Meg is appended
+ * to the significand.
+ */
+int expo_to_str(char *str, float expo)
+{
+	char *pch;
+	char buffer[MAX_CHAR];
+	char buffer2[MAX_CHAR];
+	float significand;
+	int exp;
+	int offset = 0;
+	sprintf(buffer, "%E", expo);
+	buffer[8] = 'X';
+	strcpy(buffer2, buffer);
+	significand = strtof(buffer, &pch);
+	exp = (int) strtol(pch+1, NULL, 10);
+	if(exp % 3) {
+		if(exp > 6) {
+			significand *= pow(10, exp-6);
+			exp = 6;
+		} else if(exp > 3 && exp < 6) {
+			significand *= pow(10, exp-3);	
+			exp = 3;
+		} else if(exp > 0 && exp < 3) {
+			significand *= pow(10, exp);
+			exp = 0;
+		} else if(exp > -3 && exp < 0) {
+			significand *= pow(10, exp+3);
+			exp = -3;
+		} else if(exp > -6 && exp < -3) {
+			significand *= pow(10, exp+6);
+			exp = -6;
+		} else if(exp > -9 && exp < -6) {
+			significand *= pow(10, exp+9);
+			exp = -9;
+		} else if(exp > -12 && exp < -9) {
+			significand *= pow(10, exp+12);
+			exp = -12;
+		} else if(exp > -15 && exp < -12) {
+			significand *= pow(10, exp+15);
+			exp = -15;
+		} else if(exp > -18 && exp < -15) {
+			significand *= pow(10, exp+18);
+			exp = -18;
+		}
+	}
+	sprintf(str, "%3.1f", significand);
+	if(significand < 10)
+		offset = 3;
+	else if(significand >= 10 && significand < 100)
+		offset = 4;
+	else
+		offset = 5;
+	
+	switch(exp) {
+		case -18: strcpy(str+offset, "a"); break;
+		case -15: strcpy(str+offset, "f"); break;
+		case -12: strcpy(str+offset, "p"); break;
+		case -9: strcpy(str+offset, "n"); break; //copies the sci letter right after the 3rd digit, including periods
+		case -6: strcpy(str+offset, "u"); break;
+		case -3: strcpy(str+offset, "m"); break;
+		case 0: strcpy(str+offset, ""); break;
+		case 3: strcpy(str+offset, "k"); break;
+		case 6: strcpy(str+offset, "Meg"); break;
+		default: strcpy(str+offset, ""); printf("Error in exp_to_str in switch statement!\n"); return 1;
+	}
+	return 0;
+}
+	
+/* 
+ * Converts every parameter value as given
+ * by permutations for the jth permutations
+ * into a string
+ */
+/*
+int param_expo_to_str(struct params *net, unsigned int j)
+{
+	for(int n=0; n < net->num_param; n++)
+		expo_to_str(net->ext_param_str[n], net->ext_param_permutations[j][n]);
+	return 0;
+}
+*/
+/*
+ * Updates the parameter values iteratively
+ * for each net to generate the values for
+ * ext_param_permutations
+ */
+/*
+int update_net(struct params *net)
+{	
+	net->ext_param_curr[0] += net->ext_param_step_array[0][2];
+	for(int n=0; n < net->num_param-1; n++) {
+		if(net->ext_param_curr[n] > net->ext_param_step_array[n][1]) {
+			net->ext_param_curr[n+1] += net->ext_param_step_array[n+1][2];
+			net->ext_param_curr[n] = net->ext_param_step_array[n][0];
+		}
+	}
+	if(net->ext_param_curr[net->num_param-1] > net->ext_param_step_array[net->num_param-1][1]) {
+		net->ext_param_curr[net->num_param-1] = net->ext_param_step_array[net->num_param-1][0];
+		return 1;
+	}
+	else
+		return 0;
+}
+*/
+/*
+ * Goes through the input file, tl_netlist, extracting the necessary
+ * step parameters and put them into net. Also the long pointers 
+ * store the position that the .tran, .ac, .param spice directives
+ * appear in the file, so that they can be accessed later
+ */
+int prepare_netlist(struct netlist *net, FILE* tl_netlist)
+{	
+	char buffer[MAX_CHAR];
+	char str_fl[MAX_CHAR];
+	char *p_ch;
+	char *p_tok;
+	char *p_end;
+	int param_index = 0;
+	int data_index = 0;
+	long tmp_pos = 0;
+	float tmp_data = 0;
+	
+	while(!feof(tl_netlist)) {
+		fgets(buffer, MAX_CHAR, tl_netlist);
+		p_tok = strtok(buffer, " ");
+		/*
+		if(buffer[0] == 'I') {
+			 * Assumes at most nine voltage sources,
+			 * so that the ID is only one character
+			 * Also that all of the sources are
+			 * hooked up in parallel
+			 *
+			int index = atoi(buffer+1);
+			printf("Reached a current source!\n");
+			printf("Current number: %d\n", index);
+			(*currents_pos)[index-1] = tmp_pos;
+			p_ch = buffer;
+			buffer[2] = ' ';
+			do {
+				printf("%c", *p_ch);
+				if(memcmp(p_ch-5, "PULSE", 5)==0) {
+					net->currents_index[index-1] = p_ch - buffer;
+					break;
+				}
+			} while(*(++p_ch));
+			printf("\n");
+			printf("P_ch: %s\n", p_ch);
+		}
+		*/
+		if(strcmp(buffer+1, "tran")==0) {
+			/* printf("Found tran spice directive\n"); */
+			buffer[5] = ' ';
+			strcpy(net->tran_str, buffer);
+		} else if(strcmp(buffer+1, "ac")==0) {
+			/* printf("Found ac spice directive\n"); */
+			buffer[3] = ' ';
+			strcpy(net->ac_str, buffer);
+		} else if(strcmp(buffer, ".param")==0) {
+			/* printf("Found param spice directive\n"); */
+			buffer[6] = ' ';
+			p_ch = buffer + 7;
+			printf("p_ch:%s\n", p_ch);
+			char *start, *eq_sign, *end;
+			start = eq_sign = end = NULL;
+			/*
+			 * Create comment for do { } while loop
+			 */
+			do {
+				if(*p_ch != ' ' && *(p_ch-1) == ' ')
+					start = p_ch;
+				if(*p_ch == '=')
+					eq_sign = p_ch;
+				if(*p_ch != ' ' && (*(p_ch+1) == ' ' || *(p_ch+1) == '\0'))
+					end = p_ch;
+				if(start && eq_sign && end) {
+					if(net->num_param >= net->param_size) {
+						realloc_netlist(net);
+						printf("Finished reallocation\n");
+					}
+					//printf("Copying %.*s into param_name[%d]\n", eq_sign - start, start, k);
+					sprintf(net->param_name[net->num_param], "%.*s", eq_sign - start, start);
+					//memcpy(net->param_name[k], start, eq_sign - start);
+					//printf("Copying %.*s into param_strvalue[%d]\n", end - eq_sign, eq_sign + 1, k);
+					sprintf(net->param_strvalue[net->num_param], "%.*s", end - eq_sign, eq_sign + 1);
+					//memcpy(net->param_strvalue[k], eq_sign + 1, end - eq_sign);
+					//printf("Name: %s, Value: %s\n", net->param_name[net->num_param], net->param_strvalue[net->num_param]);
+					start = eq_sign = end = NULL;
+					net->num_param++;
+				}
+			} while(*(++p_ch));
+		} else if(strcmp(buffer, ".save") == 0) {
+			//printf("Found save spice directive\n");
+			buffer[5] = ' ';
+			p_ch = strtok(NULL, " ");
+			while(p_ch != NULL) {
+				if(net->num_save >= net->save_size)
+					realloc_save(net);
+				
+				strcpy(net->save_str[net->num_save], p_ch);
+				net->num_save++;
+				p_ch = strtok(NULL, " ");
+			}
+		}
+				/*
+				for(int n=0; n < net->num_param; n++) {
+					int length = strlen(net->ext_param_name[n]);
+					if(memcmp(p_ch-length, net->ext_param_name[n], length)==0)
+						net->ext_param_index[n] = p_ch - buffer;
+				}
+				*/
+		/*
+		} else if(strcmp(buffer+1, "step")==0) {
+			strtok(NULL, " ");
+			p_tok = strtok(NULL, " ");
+			*
+			printf("Strcpy %s into net.ext_param_name[%d]\n", p_tok, net->num_param);
+			printf("currents size: %d\n", net->curr_size);
+			*
+			//strcpy(net->param_name[net->num_param], p_tok);
+			data_index = 0;
+			while(p_tok!=NULL) {
+				p_end = p_tok;
+				* printf("Strcpy p_tok: %s into str_fl...\n", p_tok); *
+				strcpy(str_fl, p_tok);
+				p_ch = str_fl;
+				do {
+					switch(*p_ch) {
+					case 'a': strcpy(p_ch, "E-18"); break;
+					case 'f': strcpy(p_ch, "E-15"); break;
+					case 'p': strcpy(p_ch, "E-12"); break;
+					case 'n':strcpy(p_ch, "E-9"); break;
+					case 'u':strcpy(p_ch, "E-6"); break;
+					case 'm':strcpy(p_ch, "E-3"); break;
+					case 'k': strcpy(p_ch, "E3"); break;
+					case 'M':strcpy(p_ch, "E6"); break;
+					}
+				} while(*(++p_ch));
+				tmp_data = strtof(str_fl, &p_end);
+				if (str_fl != p_end) {
+					net->param_step_array[net->num_param][data_index] = tmp_data;
+					data_index++;
+				}
+				p_tok = strtok(NULL, " ");
+			}
+			//net->num_param++;
+			if(net->num_param >= net->curr_size) {
+				net->curr_size = net->num_param + 3;
+				realloc_netlist(net);
+			}
+		}
+		*/
+	}
+	/*
+	for(int n=0; n < net->num_param; n++)
+		net->ext_param_curr[n] = net->ext_param_step_array[n][0];
+	
+	net->num_perm = 1;
+	printf("Generating the number of net.ext_param_permutations for .step params...\n");
+	for(int n=0; n < net->num_param; n++) {
+		net->num_perm *= (unsigned int)((net->ext_param_step_array[n][1]-net->ext_param_step_array[n][0])/net->ext_param_step_array[n][2]+1);
+	}
+	printf("Allocating memory for an array of size %u with float* to store net.ext_param_permutations...\n", net->num_perm);
+	net->ext_param_permutations = malloc(net->num_perm*sizeof(float*));
+	if(!net->ext_param_permutations)
+		printf("Not enough memory to allocate net.ext_param_permutations array\n");
+	
+	for(unsigned int i=0; i < net->num_perm; i++) {
+		net->ext_param_permutations[i] = malloc(net->num_param*sizeof(float));
+		for(int j=0; j < net->num_param; j++) {
+			net->ext_param_permutations[i][j] = net->ext_param_curr[j];
+		}
+		update_net(net);
+	}
+	for(int n=0; n < net->num_param; n++)
+		net->ext_param_curr[n] = net->ext_param_step_array[n][0];
+	
+	printf("Number of net.ext_param_permutations: %d\n", net->num_perm);
+	*/
+	return 0;
+}
+#endif /* AUTO_LTS_2_H */
---
+++ final/src/Auto_LTSpice_Now_2.c
@@ -0,0 +1,317 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<errno.h>
+#include<math.h>
+#include<ctype.h>
+
+#include"Auto_LTS_2.h"
+
+int main(int argc, char* argv[])
+{
+	/*
+	 * The one command-line argument given
+	 * is the netlist / circuit file for
+	 * spice to simulate with parameters 
+	 * stepped as provided in netlist file.
+	 * the .step directives MUST be commented
+	 * out though, or else ltsputil will mess
+	 * up the CSV files outputted. If not enough
+	 * arguments are given, exit immediately
+	 * or else argv[1] will be undefined
+	 *
+	 * ARGS: num_dropoffs, analysis, cdrp, lenline, r, l, c, signal, quoted bits with spaces
+	 */
+	if(argc < 10) {
+		printf("Not enough Command-line arguments supplied\n");
+		printf("ARGS: num_dropoffs, analysis, cdrp, lenline, r, l, c, signal, vin_arg_str\n");
+		return 1;
+	}
+	
+	struct netlist net;
+	initialize_netlist(&net, argc, argv);
+	
+	char filename_in[MAX_CHAR];
+	char filename_tmp[MAX_CHAR] = "..\\data\\tmp.cir";
+
+	char *p_ch;
+	char *p_tok;
+	char *p_end;
+	char *pch;
+	
+	long tmp_pos;
+
+	//Determine the netlist to open for reading
+	int num_dropoffs = (int) strtol(argv[1], NULL, 10);
+	sprintf(filename_in, "..\\data\\transmission_line_%d_dropoffs.cir", num_dropoffs);
+	FILE* tl_netlist = fopen(filename_in, "rb");
+	if(!tl_netlist) {
+		printf("Error in Opening Spice file, %s: %s\n", filename_in, strerror(errno));
+		return 1;
+	}
+	//Create a new temporary netlist to write changes to in case the file changes length
+	FILE* tmp_netlist = fopen(filename_tmp, "wb+");
+	if(!tmp_netlist) {
+		printf("Error in Opening or Creating Spice file, %s: %s\n", filename_tmp, strerror(errno));
+		return 1;
+	}
+	
+	/* printf("Number of currents: %d\n", net.num_currents); */
+		
+	float cdrp_arg = str_to_expo(argv[3]);
+	float lenline_arg = str_to_expo(argv[4]);
+	float r_arg = str_to_expo(argv[5]);
+	float l_arg = str_to_expo(argv[6]);
+	float c_arg = str_to_expo(argv[7]);
+	
+	char cmd_ltspice[MAX_CHAR];
+	
+	char filename_out[MAX_CHAR];
+	char filename_raw[MAX_CHAR];
+	char filename_buffer[MAX_CHAR];
+	char filename_buffer2[MAX_CHAR];
+	char buffer_str[MAX_CHAR];
+	char filename_v[MAX_CHAR];
+	char cmd_ltsputil[MAX_CHAR];
+	char cmd_cd[MAX_CHAR];
+	char cmd_mkdir[MAX_CHAR];
+	char buffer[MAX_CHAR];
+	char v_first[MAX_CHAR];
+	char v_middle[MAX_CHAR];
+	char v_last[MAX_CHAR];
+	char name_dir[MAX_CHAR];
+	
+	if(num_dropoffs == 10) {
+		strcpy(v_first, "V(n001)");
+		strcpy(v_middle, "V(n006)");
+		strcpy(v_last, "V(n010)");
+	} else if(num_dropoffs == 14) {
+		strcpy(v_first, "V(n001)");
+		strcpy(v_middle, "V(n008)");
+		strcpy(v_last, "V(n014)");
+	} else {
+		strcpy(v_first, "V(n001)");
+		strcpy(v_middle, "V(n014)");
+		strcpy(v_last, "V(n026)");
+	}
+	
+	char *v_signal[] = {v_first, v_middle, v_last, "V(out)", "V(inpulse)"};
+	int v_signal_index = 0;
+	if(strcmp(argv[8], "Vfirst")==0)
+		v_signal_index = 0;
+	else if(strcmp(argv[8], "Vmiddle")==0)
+		v_signal_index = 1;
+	else if(strcmp(argv[8], "Vlast")==0)
+		v_signal_index = 2;
+	else if(strcmp(argv[8], "Vout")==0)
+		v_signal_index = 3;
+	else if(strcmp(argv[8], "Vin")==0)
+		v_signal_index = 4;
+		
+	float progress_percent = 0;
+
+	/*
+	 * Loop through the input file looking
+	 * for the lines that specify .tran, .ac, 
+	 * .param and .step spice directives
+	 * After determining if the currents line
+	 * is any of one of those, the file stream
+	 * "cursor" is saved specifying which directive
+	 * was on that line. For the .param line the char
+	 * index of each stepped variable is saved, as to
+	 * sub in the new values each simualation net,
+	 * so the .param line MUST come after the .step
+	 * param directives in the input file
+	 */
+	printf("Preparing netlist struct...\n");
+	prepare_netlist(&net, tl_netlist);
+	/* 
+	 * step one: net all ltspice netlists with varying parameters in length, cdrp and ac vs tran
+	 * 0: tran
+	 * 1: ac
+	 */	
+	sprintf(cmd_ltspice, "scad3.exe -b %s", filename_in);
+	/* 
+	 * scad3 names the output raw file the same as the netlist file
+	 * This is important as we later rename that file to specify
+	 * what the stepped parameter values were
+	 */
+	/* Copy the schematic into tmp.cir until the voltage / current source is found,
+	 * as that is where the lengths of lines might change
+	 * This means the only lines after the voltage / current source MUST be lines
+	 * that Auto_LTSpice_Now manipulates or else they won't be copied into the
+	 * temporary netlist
+	 */
+	rewind(tl_netlist);
+	while(!feof(tl_netlist)) {
+		fgets(buffer, 255, tl_netlist);
+		if(buffer[0] == 'I' || buffer[0] == 'V')
+			break;
+		fputs(buffer, tmp_netlist);
+	}
+	/* Next comes the insertion of the voltage / current source
+	 * into the temporary netlist
+	 */
+	printf("Substitutuing input currents...\n");
+	for(int n=0; n < net.num_currents; n++) {
+		if(strcmp(net.vin_str[n], "1")==0)
+			strcpy(buffer_str, MAX_CURRENT);
+		else
+			strcpy(buffer_str, MIN_CURRENT);
+		/*
+		printf("Currents[%d]: %s\n", n, net.vin_str[n]);
+		printf("Delay[%d]: %s\n", n, net.delay[n]);
+		*/
+		sprintf(buffer, "I%d %s %s PULSE(0 %s %s %s %s %s) AC %s\n", (n+1), net.in_label, net.in_ref_label, buffer_str, net.delay[n], net.v_rise, net.v_fall, net.v_on, net.ac_amplitude);
+		printf("Completed Current Buffer: %s", buffer);
+		fputs(buffer, tmp_netlist);
+	}
+	/* Next the transient and ac analysis spice directives
+	 * are inserted into the tmp.cir netlist
+	 */
+	printf("Selecting proper analysis...\n");
+	
+	net.tran_pos = ftell(tmp_netlist);
+	/*
+	for(int n=0; n < 30; n++)
+		printf("%d", net.tran_str[n]);
+	printf("\n");
+	*/
+	fputs(net.tran_str, tmp_netlist);
+	tmp_pos = ftell(tmp_netlist);
+	if(fseek(tmp_netlist, net.tran_pos, SEEK_SET))
+		perror("Error in seeking transient analysis line");
+	fputc(net.is_tran ? '.' : ';', tmp_netlist);
+	fseek(tmp_netlist, net.ac_pos, SEEK_SET);
+	fgets(buffer, MAX_CHAR, tmp_netlist);
+	printf("Transient Analysis: %s", buffer);
+	
+	if(fseek(tmp_netlist, tmp_pos, SEEK_SET))
+		perror("Error in seeking tmp position");
+	
+	net.ac_pos = ftell(tmp_netlist);
+	fputs(net.ac_str, tmp_netlist);
+	tmp_pos = ftell(tmp_netlist);
+	/* printf("AC_str: %s", net.ac_str); */
+	if(fseek(tmp_netlist, net.ac_pos, SEEK_SET))
+		perror("Error in seeking ac analysis line");
+	fputc(net.is_tran ? ';' : '.', tmp_netlist);
+	fseek(tmp_netlist, net.ac_pos, SEEK_SET);
+	fgets(buffer, MAX_CHAR, tmp_netlist);
+	printf("AC Analysis: %s", buffer);
+	
+	if(fseek(tmp_netlist, tmp_pos, SEEK_SET))
+		perror("Error in seeking tmp position");
+	
+	/* The next line is the .param line */
+	expo_to_str(net.param_strvalue[0], cdrp_arg);
+	//expo_to_str(net.param_strvalue[1], l_arg);
+	expo_to_str(net.param_strvalue[2], lenline_arg);
+	expo_to_str(net.param_strvalue[3], r_arg);
+	expo_to_str(net.param_strvalue[4], l_arg);
+	expo_to_str(net.param_strvalue[5], c_arg);
+	printf("Substituting new paramters into netlist...\n");
+	sprintf(buffer, ".param");
+	for(int k=0; k < net.num_param; k++) {
+		/*
+		p_ch = buffer + net.ext_param_index[k];
+		pch = p_ch + 1;
+		do {
+			if((memcmp(pch, "Rline", 4)==0) || (memcmp(pch, "Lline", 4)==0) || (memcmp(pch, "Cline", 5)==0) || (memcmp(pch, "Cdrp", 4)==0)
+					|| (memcmp(pch, "Lenline", 7)==0) || (memcmp(pch, "Rterm", 4)==0)) {
+				break;
+			}
+			if(*pch != ' ' && *pch != '\n' && *pch != '\r')
+				*pch = ' ';
+		} while(*(++pch));
+		*/
+		printf(" %s=%s\n", net.param_name[k], net.param_strvalue[k]);
+		sprintf(buffer_str, " %s=%s", net.param_name[k], net.param_strvalue[k]);
+		strcat(buffer, buffer_str);
+		//sprintf(buffer_str, "%s", net.ext_param_str[k]);
+		//memcpy(p_ch+1, buffer_str, strlen(buffer_str));
+	}
+	strcat(buffer, "\n");
+	printf("Completed buffer: %s", buffer);
+	fputs(buffer, tmp_netlist);
+	
+	strcpy(buffer, ".save");
+	for(int n=0; n < net.num_save; n++) {
+		sprintf(buffer_str, " %s", net.save_str[n]);
+		strcat(buffer, buffer_str);
+	}
+	fputs(buffer, tmp_netlist);
+	fputs(".model LossyTL  LTRA(len=Lenline R=Rline L=Lline C=Cline)\n", tmp_netlist);
+	fputs(".backanno\n", tmp_netlist);
+	fputs(".end\n", tmp_netlist);
+	/* 
+	 * tl_netlist must be closed when scad3 nets since
+	 * both programs can't be updating the same file
+	 * as far as I know, as scad3 will give an error
+	 * that it can't open the input deck netlist
+	 */
+	if(fclose(tl_netlist))
+		perror("Error in closing original netlist file");
+	if(fclose(tmp_netlist))
+		perror("Error in closing temporary netlist file");
+	
+	if(remove(filename_in) != 0)
+		perror("Couldn't remove input file");
+	
+	if(rename(filename_tmp, filename_in) !=0 )
+		perror("Failed to rename tmp file as input file");
+	
+	strcpy(filename_raw, filename_in);
+	strcpy(filename_raw+strlen(filename_raw)-3, "raw");
+	sprintf(filename_buffer2, "%s", net.is_tran ? "tran" : "ac");
+	for(int n=0; n < net.num_param; n++) {
+		sprintf(filename_buffer, "_%s=%s", net.param_name[n], net.param_strvalue[n]);
+		strcat(filename_buffer2, filename_buffer);
+	}
+	sprintf(filename_buffer, "_Vin=%s", net.vin_str[0]);
+	strcat(filename_buffer2, filename_buffer);
+	for(int n=1; n < net.num_currents; n++) {
+		sprintf(filename_buffer, "_%s", net.vin_str[n]);
+		strcat(filename_buffer2, filename_buffer);
+	}
+	strcat(filename_buffer2, ".raw");
+	system(cmd_ltspice);
+	system("timeout 2");
+	strcpy(filename_in+strlen(filename_in)-4, "\0");
+
+	/* printf("file_in: %s, file_buffer2: %s\n", filename_in, filename_buffer2); */
+	sprintf(filename_out, "%s\\Multidrop%s\\%s", filename_in, net.is_tran ? "Transient" : "Bode", filename_buffer2);
+	sprintf(cmd_mkdir, "mkdir %s\\Multidrop%s", filename_in, net.is_tran ? "Transient" : "Bode");
+	system(cmd_mkdir);
+	
+	char *s = filename_out;
+	do {
+		if(*s == '/' || *s == ':' || *s == '*' || *s == '?' || *s == '"' || *s == '<' || *s == '>' || *s == '|')
+			*s = '_';
+	} while(*(++s));
+	
+	printf("filename out: %s\n", filename_out);
+	printf("filename raw: %s\n", filename_raw);
+	remove(filename_out);
+	
+	if(rename(filename_raw, filename_out)!=0)
+		perror("Failed to rename RAW file");
+	
+	strcpy(filename_raw, filename_out);
+	sprintf(filename_v, "_%s.csv", argv[8]);
+	strcpy(filename_out+strlen(filename_out)-4, filename_v);
+
+	if(net.is_tran) {
+		sprintf(cmd_ltsputil, "ltsputil.exe -xo2 %s %s \"%%14.6e\" \",\" \"#DATA\" \"time\" \"%s\"", 
+				filename_raw, filename_out, v_signal[v_signal_index]);
+				
+		system(cmd_ltsputil);
+	} else {
+		sprintf(cmd_ltsputil, "ltsputil.exe -xo2dp %s %s \"%%14.6e\" \",\" \"#DATA\" \"frequency\" \"%s\"", 
+				filename_raw, filename_out, v_signal[v_signal_index]);
+		
+		system(cmd_ltsputil);
+	}
+	return 0;
+}
